f45def7741a85bc10ec306ffa6a16e1d5236e572
AAA_shuibiao <aaashuibiao@gmail.com>
2020-08-02 11:50:01 -0400

    changed naming

--- Project/main.py
+++ Project/main.py
@@ -2,4 +2,6 @@ from read_picture import read_picture
 
 train_image, train_label = read_picture.read_image_data('mnist_data\\train-images.idx3-ubyte', 'mnist_data\\train-labels.idx1-ubyte')
 
-print(len(train_image))
+print(train_image[0])
+print(len(train_image[0]))
+print(len(train_image[0][0]))
